355fb24ff80500ea4ad7a50c49adc22d02597332
João Pedro Gonçalves <45002908+jpedro-fsgs@users.noreply.github.com>
2024-08-16 11:16:24 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <h1 className="mx-auto mt-[40vh] text-center text-7xl font-bold text-purple-900">Hello World</h1>
+  );
+}
